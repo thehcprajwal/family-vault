a5751ed29f4b6b0c8832adcc35b4fb9b6979fc68
H C Prajwal <hcprajwal.dev@gmail.com>
2026-03-21 06:19:11 +0530

    Handle homeview implementation

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -36,6 +36,28 @@
       </div>
     </div>
 
+    <!-- FAB: Upload Document -->
+    <button
+      class="fixed bottom-24 right-5 flex h-14 w-14 items-center justify-center rounded-full bg-sage shadow-lg"
+      @click="router.push('/upload')"
+      aria-label="Upload document"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <line x1="12" y1="5" x2="12" y2="19" />
+        <line x1="5" y1="12" x2="19" y2="12" />
+      </svg>
+    </button>
+
     <AppNav />
   </div>
 </template>
